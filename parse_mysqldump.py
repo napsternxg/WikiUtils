7dc8d2d15d23f604f83e390eb3a1854577d45ddc
Shubhanshu Mishra <smishra8@illinois.edu>
2019-02-20 21:53:39 -0600

    Fixed parsing issues for page and modularized code.

--- parse_mysqldump.py
+++ parse_mysqldump.py
@@ -11,11 +11,12 @@ FILEPROPS=namedtuple("Fileprops", "parser num_fields column_indexes")
 #CATEGORYLINKS_PARSER=re.compile(r'(?P<row0>[0-9]+?),(?P<row1>\'.*?\'?),(?P<row2>\'.*?\'?),(?P<row3>\'[0-9\ \-:]+\'?),(?P<row4>\'\'?),(?P<row5>\'.*?\'?),(?P<row6>\'.*?\'?)')
 CATEGORYLINKS_PARSER=re.compile(r'^(?P<row0>[0-9]+?),(?P<row1>\'.*?\'?),(?P<row2>\'.*?\'?),(?P<row3>\'[0-9\ \-:]+\'?),(?P<row4>\'.*?\'?),(?P<row5>\'[a-z\-]*?\'?),(?P<row6>\'[a-z]+\'?)$')
 PAGELINKS_PARSER=re.compile(r'^(?P<row0>[0-9]+?),(?P<row1>[0-9]+?),(?P<row2>\'.*?\'?),(?P<row3>[0-9]+?)$')
-REDIRECT_PARSER=re.compile(r'^(?P<row0>[0-9]+?),(?P<row1>[0-9]+?),(?P<row2>\'.*?\'?),(?P<row3>\'.*?\'?),(?P<row4>\'.*?\'?)$')
+REDIRECT_PARSER=re.compile(r'^(?P<row0>[0-9]+?),(?P<row1>-?[0-9]+?),(?P<row2>\'.*?\'?),(?P<row3>\'.*?\'?),(?P<row4>\'.*?\'?)$')
 CATEGORY_PARSER=re.compile(r'^(?P<row0>[0-9]+?),(?P<row1>\'.*?\'?),(?P<row2>[0-9]+?),(?P<row3>[0-9]+?),(?P<row4>[0-9]+?)$')
 PAGE_PROPS_PARSER=re.compile(r'^([0-9]+),(\'.*?\'),(\'.*?\'),(\'[0-9\ \-:]+\'),(\'\'),(\'.*?\'),(\'.*?\')$')
 PAGE_PARSER=re.compile((r'^(?P<row0>[0-9]+?),(?P<row1>[0-9]+?),(?P<row2>\'.*?\'?),(?P<row3>\'.*?\'?),(?P<row4>[0-9]+?),(?P<row5>[0-9]+?),(?P<row6>[0-9]?),'
-    r'(?P<row7>[0-9\.]+?),(?P<row8>\'.*?\'?),(?P<row9>\'.*?\'?),(?P<row10>[0-9]+?),(?P<row11>[0-9]+?),(?P<row12>(?P<row12val>\'.*?\'?)|(?P<row12null>NULL)),(?P<row13>(?P<row13val>\'.*?\'?)|(?P<row13null>NULL))$'))
+    r'(?P<row7>[0-9\.]+?),(?P<row8>\'.*?\'?),(?P<row9>(?P<row9val>\'.*?\'?)|(?P<row9null>NULL)),(?P<row10>[0-9]+?),(?P<row11>[0-9]+?),'
+    r'(?P<row12>(?P<row12val>\'.*?\'?)|(?P<row12null>NULL)),(?P<row13>(?P<row13val>\'.*?\'?)|(?P<row13null>NULL))$'))
 
 """
 # page
@@ -56,6 +57,39 @@ FILETYPE_PROPS=dict(
 
 #VALUE_PARSER=re.compile(r'\(([0-9]+),(\'.*?\'),(\'.*?\'),(\'[0-9\ \-:]+\'),(\'\'),(\'.*?\'),(\'.*?\')\)')
 
+def parse_match(match, column_indexes):
+    row = match.groupdict()
+    return tuple(row["row{}".format(i)] for i in column_indexes)
+
+
+def parse_value(value, parser, column_indexes, value_idx=0):
+    # replace unicode dash with ascii dash
+    value = value.replace("\\xe2\\x80\\x93", "-")
+    parsed_correctly = False
+    for i, match in enumerate(parser.finditer(value)):
+        parsed_correctly = True
+        try:
+            row = parse_match(match, column_indexes)
+            yield row
+        except Exception as e:
+            print("Line: {!r}, Exception: {}".format(value, e), file=sys.stderr)
+    if not parsed_correctly:
+        print("Line: {!r}, IDX: {}, Exception: {}".format(value, value_idx, "Unable to parse."), file=sys.stderr)
+
+
+def process_insert_values_line(line, parser, column_indexes, count_inserts=0):
+    start, partition, values = line.partition(' VALUES ')
+    # Each insert statement has format: 
+    # INSERT INTO "table_name" VALUES (v1,v2,v3),(v1,v2,v3),(v1,v2,v3);
+    # When splitting by "),(" we need to only consider string from values[1:-2]
+    # This ignores the starting "(" and ending ");"
+    values = values.strip()[1:-2].split("),(")
+    print("Found {} values to insert in insert no. {}".format(len(values), count_inserts), file=sys.stderr)
+    for value_idx, value in enumerate(values):
+        for row in parse_value(value, parser, column_indexes, value_idx):
+            yield row
+
+
 def process_file(fp, fp_out, filetype, column_indexes=None, silent=False):
     if filetype not in FILETYPE_PROPS:
         raise Exception("Invalid filetype: {}".format(filetype))
@@ -64,24 +98,17 @@ def process_file(fp, fp_out, filetype, column_indexes=None, silent=False):
     valid_row_keys = set(["row{}".format(i) for i in range(num_fields)])
     if column_indexes is None:
         column_indexes = ci
-    def parse_match(match):
-        row = match.groupdict()
-        return tuple(row["row{}".format(i)] for i in column_indexes)
     with tqdm(disable=silent) as pbar:
-        for i, line in enumerate(fp):
+        count_inserts = 0
+        for line_no, line in enumerate(fp, start=1):
             if line.startswith('INSERT INTO `{}` VALUES '.format(filetype)):
-                start, partition, values = line.partition(' VALUES ')
-                # replace unicode dash with ascii dash
-                values = values.strip()[1:-1].split("),(")
-                for value in values:
-                    value = value.replace("\\xe2\\x80\\x93", "-")
-                    for i, match in enumerate(parser.finditer(value)):
-                        try:
-                            pbar.update(1)
-                            row = parse_match(match)
-                            print("\t".join(row), file=fp_out)
-                        except Exception as e:
-                            print("Line: {}, Exception: {}".format(i, e), file=sys.stderr)
+                count_inserts += 1
+                for row in process_insert_values_line(
+                        line, parser, column_indexes, count_inserts):
+                    if pbar is not None:
+                        pbar.update(1)
+                    print("\t".join(row), file=fp_out)
+
 
 def main():
     import argparse
